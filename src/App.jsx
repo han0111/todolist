c593fbfbabd39bcb70b572b3c103316b6f8644fd
Han Hee <hanheeww@naver.com>
2023-06-15 17:41:31 +0900

    doneTitle,Comment을 title,comment로 변경,usestate부분 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -46,10 +46,10 @@ function App() {
 
   //Done 컨테이너 만들기
   const [donePlans, setDonePlans] = useState(  
-    [{id:1, doneTitle: "리액트공부하기 완료", doneComment: "리액트 기초를 공부해봅시다 완료"},
+    [{id:1, title: "리액트공부하기 완료", comment: "리액트 기초를 공부해봅시다 완료"},
   ]);
-  const [donetitle, setDoneTitle] = useState("");
-  const [doneComment, setDoneComment] = useState("");
+  // const [donetitle, setDoneTitle] = useState("");
+  // const [doneComment, setDoneComment] = useState("");
 
   //Done 컨테이너 삭제 버튼 변수할당 및 카드삭제
   const clickDoneRemoveButtonHandler = (id) => {
@@ -96,8 +96,8 @@ function App() {
           plans.map((item)=>{
             return (
               <div key={item.id} className="component-style">
-                <h2>{item.title}{item.doneTitle}</h2>
-                <div>{item.comment}{item.doneComment}</div>
+                <h2>{item.title}</h2>
+                <div>{item.comment}</div>
                 <div className="btn">
                 <button className="red-btn" onClick={() => clickRemoveButtonHandler(item.id)}>삭제하기</button>
                 <button className="green-btn" onClick={() => clickCompleteButtonHandler(item.id)}>완료</button>
@@ -114,8 +114,8 @@ function App() {
         {donePlans.map((item)=>{
           return (
             <div key={item.id} className="component-style">
-            <h2>{item.doneTitle}{item.title}</h2>
-            <div>{item.doneComment}{item.comment}</div>
+            <h2>{item.title}</h2>
+            <div>{item.comment}</div>
             <div className="btn">
             <button className="red-btn" onClick={() => clickDoneRemoveButtonHandler(item.id)}>삭제하기</button>
             <button className="green-btn" onClick={() => clickDeleteButtonHandler(item.id)}>취소</button>
